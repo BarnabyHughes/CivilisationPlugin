268d62d9d49fa6c2a75fe32ad3a2499f73181a3c
barnabyhughes <barneyhughes10@outlook.com>
2025-02-13 10:29:14 +0000

    Github

--- src/main/java/me/barnaby/civilisation/util/StaffUtil.java
+++ src/main/java/me/barnaby/civilisation/util/StaffUtil.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public class StaffUtil {
 
+    /**
+     * Retrieves a list of online players who have the permission "civilisation.notified".
+     *
+     * @return a List of Player objects representing the online staff members who meet the permission criteria.
+     */
     public static List<Player> getOnlineStaff() {
         List<Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
         players.removeIf(player -> !player.hasPermission("civilisation.notified"));
